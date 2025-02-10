43dfd120fa0b11dd99e02524fe8e2dfd25619f0f
Batalla95 <152439623+Batalla95@users.noreply.github.com>
2025-02-10 14:22:33 +0100

    Character controller
    Primera etapa de character controller

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public Rigidbody rB;
+    public Vector3 InputKey;
+    float MyFloat;
+    
+    void Update()
+    {
+        InputKey = new Vector3(Input.GetAxis("horizontal"), 0, Input.GetAxis("vertical"));
+    }
+
+    private void FixedUpdate()
+    {
+        rB.MovePosition((Vector3)transform.position + InputKey * 10 * Time.deltaTime);
+
+        float Angle = Mathf.Atan2(InputKey.x, InputKey.z) * Mathf.Rad2Deg;
+        float Smooth = Mathf.SmoothDampAngle(transform.eulerAngles.y, Angle, ref MyFloat, 0.1f);
+
+        transform.rotation=Quaternion.Euler(0, Angle,0);
+    }
+}
