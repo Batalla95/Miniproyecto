a437d6eeb8236ea4e8362a4e09c2a54a7e706a24
Batalla95 <152439623+Batalla95@users.noreply.github.com>
2025-03-12 13:55:57 +0100

    Sistema de munición/recarga
    Se ha implementado el sistema de munición t recarga

--- Assets/Scripts/Gun.cs
+++ Assets/Scripts/Gun.cs
@@ -5,40 +5,80 @@ public class Gun : MonoBehaviour
 {
     public UnityEvent OnGunShoot;
     public float FireCooldown;
+    public float RechargeCooldown;
+    public float MaxAmmo;
+    public float MaxMag;
 
     public bool Automatic;
+    public bool InfinityAmmo;
 
     private float CurrentCooldown;
+    private float CurrentRecharge;
+    [SerializeField] private float CurrentMaxAmmo;
+    [SerializeField] private float CurrentMaxMag;
+
 
     private void Start()
     {
         CurrentCooldown = FireCooldown;
+        CurrentRecharge = RechargeCooldown;
+        CurrentMaxAmmo = MaxAmmo;
+        CurrentMaxMag = MaxMag;
+        
     }
 
     private void Update()
     {
+
+        if (Input.GetKeyDown(KeyCode.R)||CurrentMaxMag==0)
+        {
+            Recharge();
+            CurrentCooldown = CurrentRecharge;
+        }
+        
+        
         if (Automatic)
         {
-            if (Input.GetButtonDown("Fire1"))
+            if (Input.GetButton("Fire1"))
             {
-                if (CurrentCooldown <= 0)
+                if (CurrentCooldown <= 0 && CurrentMaxMag>0)
                 {
                     OnGunShoot?.Invoke();
                     CurrentCooldown = FireCooldown;
+                    CurrentMaxMag -= 1;
+                    
                 }
+               
             }
         }
         else
         {
             if (Input.GetButtonDown("Fire1"))
             {
-                if (CurrentCooldown <= 0)
+                if (CurrentCooldown <= 0 && CurrentMaxMag > 0)
                 {
                     OnGunShoot?.Invoke();
                     CurrentCooldown = FireCooldown;
+                    CurrentMaxMag -= 1;
                 }
+                
             }
         }
         CurrentCooldown -= Time.deltaTime;
+        CurrentRecharge -= Time.deltaTime;
+    }
+
+
+    public void Recharge()
+    {
+        if (InfinityAmmo)
+        {
+            CurrentMaxMag = MaxMag;
+        }
+        else if(CurrentMaxAmmo>0)
+        {
+            CurrentMaxAmmo -= MaxMag;
+            CurrentMaxMag = MaxMag;
+        }
     }
 }
